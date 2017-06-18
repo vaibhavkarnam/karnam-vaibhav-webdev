769810acd3871b1fe72785687069cb74d35e7175
vaibhavkarnam <karnamvaibhav360@gmail.com>
2017-06-18 11:48:15 -0400

    assignment6 update

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -39,7 +39,7 @@ app.get ('/auth/facebook', passport.authenticate('facebook', { scope : ['email']
 app.get('/auth/facebook/callback',
     passport.authenticate('facebook', {
         successRedirect: '/assignment/index.html#!/user/profile',
-        failureRedirect: '/assignment/index.html#!/login',
+        failureRedirect: '/assignment/index.html#!/login'
     }));
 
 
