41c180782b5a73baed6746918f76c63b2691d291
vaibhavkarnam <karnamvaibhav360@gmail.com>
2017-07-12 13:37:33 -0400

    test

--- project/app.js
+++ project/app.js
@@ -1,20 +1,20 @@
-/**
- * Created by vaibhav on 15-06-2017.
- */
-var app = require('../express');
-var mongoose = require("mongoose");
-//mongoose.connect("mongodb://localhost:27017/webdev");
-//var db = mongoose.connection;
-//db.on('error', console.error.bind(console, 'connection error:'));
-//db.once('open', function() {
-//    console.log("connected")
-//});
-mongoose.Promise=require("q").Promise;
-require('./services/user.service.server');
-//require('./services/website.service.server');
-//require('./services/widget.service.server');
-
-
-require('./services/search-address.service.server');
-require('./xml2json');
-
+// /**
+//  * Created by vaibhav on 15-06-2017.
+//  */
+// var app = require('../express');
+// var mongoose = require("mongoose");
+// //mongoose.connect("mongodb://localhost:27017/webdev");
+// //var db = mongoose.connection;
+// //db.on('error', console.error.bind(console, 'connection error:'));
+// //db.once('open', function() {
+// //    console.log("connected")
+// //});
+// mongoose.Promise=require("q").Promise;
+// require('./services/user.service.server');
+// //require('./services/website.service.server');
+// //require('./services/widget.service.server');
+//
+//
+// require('./services/search-address.service.server');
+// require('./xml2json');
+//
