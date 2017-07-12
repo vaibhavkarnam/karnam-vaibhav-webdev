7386e3eecf317c383a2a78cfc595900cded24823
vaibhavkarnam <karnamvaibhav360@gmail.com>
2017-07-12 13:15:53 -0400

    removed test

--- assignment/model/user/user.schema.server.js
+++ assignment/model/user/user.schema.server.js
@@ -1,22 +1,22 @@
-// var mongoose = require('mongoose');
-//
-// var userSchema = mongoose.Schema({
-//     username: {type: String, unique: true},
-//     password: String,
-//     firstName: String,
-//     lastName: String,
-//     email: String,
-//     roles: [{type: String, default: 'USER', enum: ['USER', 'STUDENT', 'FACULTY', 'ADMIN']}],
-//     google:{
-//         id: String,
-//         token: String
-//     },
-//     facebook: {
-//         id:    String,
-//         token: String
-//     },
-//     websites: [{type: mongoose.Schema.Types.ObjectId, ref: "GraduateWebsiteModel"}],
-//     dateCreated: {type: Date, default: Date.now}
-// }, {collection: "user"});
-//
-// module.exports = userSchema;
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    username: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    roles: [{type: String, default: 'USER', enum: ['USER', 'STUDENT', 'FACULTY', 'ADMIN']}],
+    google:{
+        id: String,
+        token: String
+    },
+    facebook: {
+        id:    String,
+        token: String
+    },
+    websites: [{type: mongoose.Schema.Types.ObjectId, ref: "GraduateWebsiteModel"}],
+    dateCreated: {type: Date, default: Date.now}
+}, {collection: "user"});
+
+module.exports = userSchema;
